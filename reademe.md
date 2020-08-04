d4f54424d4bc5138fe77adbad59ea3a0421b1afc
zz966 <zz966@uowmail.edu.au>
2020-08-05 06:40:36 +1000

    Initial commit

---
+++ reademe.md
@@ -0,0 +1,20 @@
+**Weather app**  
+This is a weather forcast application.
+
+This project inteact with **mapbox api** and **darksky api**.  input location to get the weather's information of the location.     
+
+But this project is **deprecated** as **dark sky api service is going to shut down.**  
+![image](screenshot/Dark_Sky.png)
+
+the screenshot of the project:
+![image](screenshot/screenshot1.png)
+
+
+
+**How to run the project**  
+nodemon src/app.js
+
+using mapbox require an access token, so you need to create an account.
+1. signup an account on mapbox  
+2. mapbox api documentaion  
+https://docs.mapbox.com/api/search/#geocoding  
---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token={replace with you own access token}'
+
+    request({ url, json: true }, (error, { body }) => {
+       // console.log(body)
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+           
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            console.log("234")
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
